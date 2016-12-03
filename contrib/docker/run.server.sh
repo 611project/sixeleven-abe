0cafeb5c3699cab6586f1e113a4d600df36d7fb0
Florian Fuessl <flo@degnet.de>
2016-12-03 04:21:13 +0100

    docker bugfix

--- contrib/docker/run.server.sh
+++ contrib/docker/run.server.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # initial setup of sqlite
-python -m Abe.abe --config sqlite.conf --no-serve
+python -m Abe.abe --config sqlite.conf --commit-bytes 100000 --no-serve
+# rescan after initial setup
+python -m Abe.abe --config sqlite.conf --no-serve --rescan
 
 # update the database once a minute in the background
 while true
