b46b229cc0acc780db87cc1a03d78844cf7135bb
Florian Fuessl <flo@degnet.de>
2017-12-17 06:39:44 +0100

    DoS prevention

--- contrib/docker/run.server.sh
+++ contrib/docker/run.server.sh
@@ -2,10 +2,10 @@
 
 # initial setup of sqlite
 python -m Abe.abe --config sqlite.conf --commit-bytes 100000 --no-serve
-# rescan after initial setup
-python -m Abe.abe --config sqlite.conf --no-serve --rescan
 
-# update the database once a minute in the background
+# rescan after initial setup and
+# update afterwards the database once a minute in the background
+python -m Abe.abe --config sqlite.conf --no-serve --rescan && \
 while true
 do
   python -m Abe.abe --config sqlite.conf --no-serve --upgrade
